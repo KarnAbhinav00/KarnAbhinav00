b5347726b56e50aa86d37a6388dc11293caa70ab
Abhinav <kazama.social@gmail.com>
2025-08-20 22:03:46 +0530

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:5000/api/projects"; // backend ka route
-
-export const fetchProjects = () => axios.get(API_URL);
-export const createProject = (project) => axios.post(API_URL, project);
-export const updateProject = (id, updates) => axios.put(`${API_URL}/${id}`, updates);
-export const deleteProject = (id) => axios.delete(`${API_URL}/${id}`);
